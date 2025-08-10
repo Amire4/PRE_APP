7d16a1417ff60826b9331a6d6c92d22182e1eb0b
Rana Amir Shahzad <ranaamirshahzad630@gmail.com>
2025-08-10 19:17:38 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Practice Essentials
+
+This is my first practice app in the course
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+ 
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className='mb-3'>Hello World</h1>
+       <p>Hi Rana Amir Shahzad</p>
+    </>
+  )
+}
+
+export default App
